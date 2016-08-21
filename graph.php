7401fd147727dbb839d9ab8e8d0f3fa056635497
ckcompton <ckcompton@yahoo.com>
2016-08-21 18:37:00 -0500

    working version of graph

--- graph.php
+++ graph.php
@@ -2,50 +2,43 @@
 require_once ('src/jpgraph.php');
 require_once ('src/jpgraph_line.php');
 
-     $servername = "localhost";
-     $username = "root";
-     $password = "football12";
-     $dbname = "readings";
-     $conn = new mysqli($servername, $username, $password, $dbname);
+$servername = "localhost";
+$username = "root";
+$password = "football12";
+$dbname = "readings";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
      // Check connectionwe fawefawef
-     $results= array();
-     if ($conn->connect_error) {
-     	$results[2]=12.0;
-       die("Connection failed: " . $conn->connect_error);
-     }
-     
-     $sql = mysql_query("SELECT * FROM data ORDER BY data.time DESC LIMIT 10");
-     while($row = mysql_fetch_row($sql))
+$results= array();
+$results2 = array();
+if ($conn->connect_error) {
+	$results[2]=12.0;
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM data ORDER BY data.time DESC LIMIT 10";
+
+
+$sqls = $conn->query($sql);
+
+	while($row = $sqls->fetch_assoc())
 	{
-   $results[] = $row['temperature'];
-	}
-     //$checkquestion = mysql_query($sql);
-
-	if (!$checkquestion) {
-    $results[1]=10.0;
-	}
-	if ($checkquestion) {
-    $results[1]=20.0;
-	}
-	if (!$sql) {
-    $results[3]=30.0;
-	}
-	if ($sql) {
-    $results[1]=100.0;
+		
+		$results2[]=(double)$row['temperature'];
 	}
 
 
-//$ydata = array((double)$results[0],(double)$results[1],(double)$results[2],(double)$results[3],(double)$results[4],(double)$results[5]);
+$ydata = array(trim($results[0]),trim($results[1]),$results[2],$results[3],$results[4],$results[5]);
 // Some data
-$ydata = array(11,3,8,12,5,1,9,13,5,7);
+//$ydata = array(11,3,8,12,5,1,9,13,5,7);
 
 // Create the graph. These two calls are always required
-$graph = new Graph(350,250);
-$graph->SetScale('textlin');
+$graph = new Graph(800,600);
+$graph->SetScale('textlin',0,4);
 
 // Create the linear plot
-$lineplot=new LinePlot($ydata);
+//$lineplot=new LinePlot($ydata);
+$lineplot=new LinePlot($results2);
 $lineplot->SetColor('blue');
 
 // Add the plot to the graph
--- index.php
+++ index.php
@@ -14,39 +14,54 @@
      $password = "football12";
      $dbname = "readings";
      $conn = new mysqli($servername, $username, $password, $dbname);
+
+
      // Check connectionwe fawefawef
      if ($conn->connect_error) {
        die("Connection failed: " . $conn->connect_error);
      }
-     $sql = mysql_query("SELECT * FROM data ORDER BY data.time DESC LIMIT 10");
+     $sql = "SELECT * FROM data ORDER BY data.time DESC LIMIT 10";
 
-     $result = $conn->query($sql);
-     if (!$result) {
-      throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
-    }
-    if($result = $conn->query($sql)){
+
+     if($result = $conn->query($sql)){
 
       $results= array();
+      echo "Voltage: ";
+      while($row = $result->fetch_assoc()) {
 
- while($row = $result->fetch_assoc()) {
-   echo "Voltage: " . $row["temperature"]. "<br>";
- }
-}
-$sql = "SELECT table_name AS 'data', ROUND(((data_length + index_length) / 1024 / 1024), 2) AS 'Size (MB)' FROM information_schema.TABLES WHERE table_schema = 'readings' ORDER BY (data_length + index_length) DESC;";
-$sizeOfTable = $conn->query($sql);
-if ($sizeOfTable->num_rows > 0) {
+       echo $row["temperature"]. " ";
+     }
+
+   }
+
+ $sql = "SELECT table_name AS 'data', ROUND(((data_length + index_length) / 1024 / 1024), 2) AS 'Size (MB)' FROM information_schema.TABLES WHERE table_schema = 'readings' ORDER BY (data_length + index_length) DESC;";
+ $sizeOfTable = $conn->query($sql);
+ if ($sizeOfTable->num_rows > 0) {
    // output data of each row
- while($row = $sizeOfTable->fetch_assoc()) {
-   echo "Size of database " . $row["Size (MB)"]. "MB". "<br>";
- }
-} else {
+   while($row = $sizeOfTable->fetch_assoc()) {
+     echo "Size of database " . $row["Size (MB)"]. "MB". "<br>";
+   }
+ } else {
   echo "0 results";
 }
 
 $conn->close();
 //<img src="graph.php"> 
 
- /*echo "Results[0]" . (double)$results[0] ."<br>";
+ /*
+
+ $sqls = $conn->query($sql);
+for($i=0; $i<=($sqls->num_rows); $i++)
+{
+  while($row = $sqls->fetch_assoc())
+ {
+   $results[i] = $row['temperature'];
+   //echo $row['temperature'] . "<br>";
+   echo $results[i] . "<br>";
+   }
+ }
+
+ echo "Results[0]" . (double)$results[0] ."<br>";
  echo "Results[1]" . (double)$results[1] ."<br>";
  echo "Results[2]" . (double)$results[2] ."<br>";
  echo "Results[3]" . (double)$results[3] ."<br>";
@@ -56,11 +71,16 @@ $conn->close();
   while($row = mysql_fetch_assoc($sql))
  {
    $results[] = $row['temperature'];
+   echo $row['temperature'] . "<br>";
  }
+ if (!mysql_select_db("readings")) {
+    echo "Unable to select mydbname: " . mysql_error();
+    exit;
+}
 */
 
 ?>
-
+<img src="graph.php"> 
 </body>
 </html>
 
