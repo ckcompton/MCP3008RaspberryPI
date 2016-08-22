b2adb3d9b3eab12dab3a8c4f658bf71511f6e223
ckcompton <ckcompton@yahoo.com>
2016-08-22 14:22:56 -0500

    works

--- graph.php
+++ graph.php
@@ -4,15 +4,13 @@ require_once ('src/jpgraph_line.php');
 
 $servername = "localhost";
 $username = "root";
-$password = "football12";
+$password = "###";
 $dbname = "readings";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
      // Check connectionwe fawefawef
-$results= array();
-$results2 = array();
+$results = array();
 if ($conn->connect_error) {
-	$results[2]=12.0;
 	die("Connection failed: " . $conn->connect_error);
 }
 
@@ -20,7 +18,7 @@ $sql = "SELECT * FROM data ORDER BY data.time DESC LIMIT 10";
 $sqls = $conn->query($sql);
 	while($row = $sqls->fetch_assoc())
 	{
-		$results2[]=(double)$row['temperature'];
+		$results[]=(double)$row['temperature'];
 	}
 
 // Create the graph. These two calls are always required
@@ -29,7 +27,7 @@ $graph->SetScale('textlin',0,4);
 
 // Create the linear plot
 //$lineplot=new LinePlot($ydata);
-$lineplot=new LinePlot($results2);
+$lineplot=new LinePlot($results);
 $lineplot->SetColor('blue');
 
 // Add the plot to the graph
--- index.php
+++ index.php
@@ -4,9 +4,13 @@
       <title>Reading Voltage from Database</title>
     </head>
     <body>
+
+
     <img src="graph.php"> 
+     
      <?php
 
+      //all graphing to JPgraph
      require_once ('src/jpgraph.php');
      require_once ('src/jpgraph_line.php');
 
@@ -21,20 +25,22 @@
      if ($conn->connect_error) {
        die("Connection failed: " . $conn->connect_error);
      }
+
+     //gets the last 10 voltage recordings from the database sorted by the time
      $sql = "SELECT * FROM data ORDER BY data.time DESC LIMIT 10";
 
 
      if($result = $conn->query($sql)){
 
       $results= array();
-      echo "Voltage: ";
+      echo "<br>" . "Voltage: ";
       while($row = $result->fetch_assoc()) {
 
        echo $row["temperature"]. " ";
      }
 
    }
-
+//Gets the size (MB) of the database
  $sql = "SELECT table_name AS 'data', ROUND(((data_length + index_length) / 1024 / 1024), 2) AS 'Size (MB)' FROM information_schema.TABLES WHERE table_schema = 'readings' ORDER BY (data_length + index_length) DESC;";
  $sizeOfTable = $conn->query($sql);
  if ($sizeOfTable->num_rows > 0) {
@@ -48,6 +54,9 @@
 
 $conn->close();
 
+ $page = $_SERVER['PHP_SELF'];
+ $sec = "1"; //time in seconds to refresh page
+ header("Refresh: $sec; url=$page"); //refreshes the page every one second
 
 ?>
 
